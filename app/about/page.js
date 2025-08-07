56393bf02d6f4090c32b5e5a70912bce463ed9fb
Danialmsh <danial.moshtaghi98@gmail.com>
2025-08-07 15:56:06 +0330

    First

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div id="nasrinWrapper">
-      <h1 className="h4">درباره ArchiVision</h1>
+      <h1 className="h4">درباره UPSaze</h1>
       <p>
         ما گروهی از معماران و طراحان هستیم که با تلفیق هنر و فناوری،
         فضاهایی ماندگار و کارآمد خلق می‌کنیم…
